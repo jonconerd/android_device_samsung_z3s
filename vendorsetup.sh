971c154ad5ce959c31d831add19e75ebab9fe962
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-07-27 19:30:11 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,3 +7,12 @@
 
 # For building with minimal manifest
 export ALLOW_MISSING_DEPENDENCIES=true
+export FOX_DYNAMIC_SAMSUNG_FIX=1
+export FOX_REMOVE_AAPT=1
+export FOX_DELETE_MAGISK_ADDON=1
+export OF_USE_MAGISKBOOT=0
+export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=0
+export OF_RUN_POST_FORMAT_PROCESS=1
+export OF_FL_PATH1="/tmp/flashlight"
+export OF_FL_PATH2=""
+export OF_FLASHLIGHT_ENABLE=1
