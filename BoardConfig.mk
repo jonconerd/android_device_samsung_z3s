de8454101f78fd46285177b646c6fa8137bb72c2
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-07-27 17:17:16 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -145,6 +145,8 @@ TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
 TW_INCLUDE_CRYPTO := true
 TW_FORCE_KEYMASTER_VER := true
+TW_INCLUDE_CRYPTO_FBE := true
+TW_INCLUDE_FBE_METADATA_DECRYPT := true
 
 #PBRP specific
 PB_TORCH_PATH := "/sys/devices/virtual/camera/flash/rear_flash"
