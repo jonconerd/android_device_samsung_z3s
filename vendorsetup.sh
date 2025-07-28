9213b632df88174e7411b1d95168888a581b0de5
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-07-28 04:43:14 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -22,7 +22,8 @@ export FOX_DYNAMIC_SAMSUNG_FIX=1
 export FOX_DELETE_MAGISK_ADDON=1
 export OF_USE_MAGISKBOOT=0
 export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=0
-export OF_FL_PATH1="/tmp/flashlight"
-export OF_FL_PATH2=""
-export OF_FLASHLIGHT_ENABLE=1
 export OF_RUN_POST_FORMAT_PROCESS=1
+export OF_FL_PATH1="/tmp/flashlight"
+        export OF_FL_PATH2=""
+        export OF_FLASHLIGHT_ENABLE=1
+
